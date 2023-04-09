40b2b6eec09697c0987b48e14a40e705eac386a8
Matthieu Mary <matthieu.mary2@gmail.com>
2023-04-09 16:02:33 +0200

    improve logic for ProtectedRoute by using the token saved in storage

--- frontend/src/router/ProtectedRoutes.tsx
+++ frontend/src/router/ProtectedRoutes.tsx
@@ -1,11 +1,9 @@
-import { useSelector } from "react-redux"
-import { RootState } from "../app/store"
 import { Outlet } from "react-router-dom"
 import ErrorPage from "../pages/ErrorPage"
 
 
 function ProtectedRoutes() {
-    const userConnected = useSelector((state: RootState) => state.auth.authSuccess)
+  const userConnected = localStorage.getItem("token") ? localStorage.getItem("token") : sessionStorage.getItem("token") ? sessionStorage.getItem("token") : null;
     
   return userConnected ? <Outlet /> : <ErrorPage message='Please login to access the profile page' />
 }
